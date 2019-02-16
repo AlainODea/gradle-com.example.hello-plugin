d7a21432311c2d07a29a590638eed71fe3ef50a5
Alain O'Dea <alain.odea@gmail.com>
2019-02-16 14:02:57 -0330

    :art: Use Kotlin DSL for tidier code
     - Apply kotlin-dsl plugin
    
     - Import it explicitly in implementation
    
     - Tidy up code with new-found DSL powers

--- src/main/kotlin/com/example/HelloPlugin.kt
+++ src/main/kotlin/com/example/HelloPlugin.kt
@@ -3,13 +3,14 @@ package com.example
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Exec
+import org.gradle.kotlin.dsl.*
 
 class HelloPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.afterEvaluate {
-            project.tasks.register("hello", Exec::class.java) { task ->
-                task.commandLine = listOf(
+            tasks.register<Exec>("hello") {
+                commandLine = listOf(
                     "echo",
                     "Hello, world!"
                 )
